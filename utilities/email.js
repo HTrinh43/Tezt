552ad7e961c4c506772e46206afe7eba84e24050
cordelh <cordelh@uw.edu>
2021-11-09 09:58:42 -0800

    Escaped the quotation marks.

--- utilities/email.js
+++ utilities/email.js
@@ -36,8 +36,8 @@ let sendEmail = (sender, receiver, subject, token) => {
         subject: subject,
         html: `<h1>Welcome to our chat app!</h1>
             <p>Please press the button below to verify your email address.</p>
-            <form action ="https://tcss450-group4-project.herokuapp.com/verify?id=${token}">
-                <button type ="submit" value="Verify Email Address"/>
+            <form action =\"https://tcss450-group4-project.herokuapp.com/verify?id=${token}\">
+                <button type =\"submit\" value=\"Verify Email Address\" />
             </form>
             <p>If you did not create an account, no further action is required.</p>
             <p>Best regards,</p>
